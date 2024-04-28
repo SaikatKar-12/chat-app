4d554c3718190148792ef8bd95f7db8ae5500563
Saikat Kar <ksaikat009@gmail.com>
2024-04-28 08:29:07 +0530

    added mongo atlas

--- config/database-config.js
+++ config/database-config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connect = async () => {
-    await mongoose.connect("mongodb://localhost/chatapp");
+    await mongoose.connect("mongodb+srv://ksaikat009:gHt0UA5Q35r1JzsR@cluster0.euybszc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 }
 
 module.exports = connect;
